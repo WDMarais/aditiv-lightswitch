2050ec6b9c18e8d3ff7142234be3dfc87031db51
Wynand Marais <marais.wynand@gmail.com>
2022-04-25 07:41:14 +0200

    Add node backend config

--- server/server.ts
+++ server/server.ts
@@ -1,2 +1,8 @@
-const hello = 'Hello world';
-console.log(hello);
+import * as express from 'express';
+
+const app = express();
+let port = 4201;
+let host = '127.0.0.1';
+app.listen(port, host, () => {
+    console.log('Node server is listening');
+});
