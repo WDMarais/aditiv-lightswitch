f1c9025192112b6b4f694ed4155d34edd927318b
Wynand Marais <marais.wynand@gmail.com>
2022-04-25 08:32:15 +0200

    Add basic example routes to server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,24 @@
-import * as express from 'express';
+const express = require('express');
 
 const app = express();
+
+app.use((req: any, res: any, next: any) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    } else {
+        console.log(`${req.ip} ${req.method} ${req.url}`);
+        next();
+    }
+})
+
+app.get('/', (req: any, res: any) => {
+    let response = { hello: 'world' };
+    res.send(response);
+})
+
 let port = 4201;
 let host = '127.0.0.1';
 app.listen(port, host, () => {
