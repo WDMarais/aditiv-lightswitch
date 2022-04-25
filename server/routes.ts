2a31f1d690cf3a80de8e90b5d7ebeb56932a617e
Wynand Marais <marais.wynand@gmail.com>
2022-04-25 09:24:37 +0200

    Minor route fix

--- server/routes.ts
+++ server/routes.ts
@@ -3,6 +3,6 @@ const app = express.Router();
 
 export { app as routes };
 
-app.get('/', (req: any, res: any) => res.send('Hello World'));
+app.get('/', (req: any, res: any) => res.send([{message: 'Hello World'}]));
 app.get('/users', (req: any, res: any) => res.send([]));
 app.post('/users', (req: any, res: any) => res.send({body: req.body}));
